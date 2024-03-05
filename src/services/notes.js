382f5a82bf4059d76a9eb2cb7292a9ea72fe8369
JimTK16 <longduy88@gmail.com>
2024-03-05 22:43:56 +1100

    fix base Url

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://fso-part03-backend.onrender.com'
+const baseUrl = 'https://fso-part03-backend.onrender.com/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
