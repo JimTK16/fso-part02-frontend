9ee7b5c962e0613ebfef3c11c9a06ff19d98a707
JimTK16 <longduy88@gmail.com>
2024-03-06 09:44:32 +1100

    vite config for proxy, change base url

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://fso-part03-backend.onrender.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
